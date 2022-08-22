cb02f823681921efe2e08395b90c1380438f39f1
Matt Thurmond <matt.thurmond@gmail.com>
2022-08-22 16:20:53 -0400

    automate adding characters and add ~15 more

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // add image to page for each character
+    const characters = [
+        'banon',
+        'celes',
+        'cyan',
+        'edgar', 
+        'gau',
+        'gogo',
+        'kefka',
+        'leo', 
+        'locke',
+        'madeline',
+        'maduin',
+        'mog',
+        'relm',
+        'sabin',
+        'setzer',
+        'shadow',
+        'strago',
+        'terra', 
+        'umaro',
+    ]
+    let html = ''
+    for (let i = 0; i < characters.length; i++) {
+        const character = characters[i]
+        html = html + `<div class="p-4"><a href="/${character}"><img class="character-image" src="/images/${character}.jpg" alt="${character}" data-character="${character}"></a></div>`
+    }
+    const imageContainer = document.getElementsByClassName('write-html')[0]
+    imageContainer.innerHTML = html
+
     // change image on hover
-    let images = document.getElementsByClassName('character-image')
+    const images = document.getElementsByClassName('character-image')
     for (i = 0; i < images.length; i++) {
         let image = images[i]
         let character = image.dataset.character
