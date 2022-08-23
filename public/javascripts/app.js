8d0b922c9a523c16ea2b2d2f99d367a45af23916
Matt Thurmond <matt.thurmond@gmail.com>
2022-08-23 09:45:08 -0400

    only attempt to add all sprites if user on /index

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,44 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // add image to page for each character
-    const characters = [
-        'banon',
-        'celes',
-        'cyan',
-        'edgar', 
-        'gau',
-        'gogo',
-        'kefka',
-        'leo', 
-        'locke',
-        'madeline',
-        'maduin',
-        'mog',
-        'relm',
-        'sabin',
-        'setzer',
-        'shadow',
-        'strago',
-        'terra', 
-        'umaro',
-    ]
-    let html = ''
-    for (let i = 0; i < characters.length; i++) {
-        const character = characters[i]
-        html = html + `<div class="p-4"><a href="/${character}"><img class="character-image" src="/images/${character}.jpg" alt="${character}" data-character="${character}"></a></div>`
-    }
+    // when user navigates to index page, add image for each character
     const imageContainer = document.getElementsByClassName('write-html')[0]
-    imageContainer.innerHTML = html
-
-    // change image on hover
-    const images = document.getElementsByClassName('character-image')
-    for (i = 0; i < images.length; i++) {
-        let image = images[i]
-        let character = image.dataset.character
-        image.onmouseout = function() {
-            image.src = `/images/${character}.jpg`
+    if (imageContainer) {
+    
+        const characters = [
+            'banon',
+            'celes',
+            'cyan',
+            'edgar',
+            'gau',
+            'gogo',
+            'kefka',
+            'leo',
+            'locke',
+            'madeline',
+            'maduin',
+            'mog',
+            'relm',
+            'sabin',
+            'setzer',
+            'shadow',
+            'strago',
+            'terra',
+            'umaro',
+        ]
+        let html = ''
+        for (let i = 0; i < characters.length; i++) {
+            const character = characters[i]
+            html = html + `<div class="p-4"><a href="/${character}"><img class="character-image" src="/images/${character}.jpg" alt="${character}" data-character="${character}"></a></div>`
         }
-        image.onmouseover = function() {
-            image.src = `/images/${character}-wave.jpg`
+        imageContainer.innerHTML = html
+
+        // change image on hover
+        const images = document.getElementsByClassName('character-image')
+        for (i = 0; i < images.length; i++) {
+            let image = images[i]
+            let character = image.dataset.character
+            image.onmouseout = function() {
+                image.src = `/images/${character}.jpg`
+            }
+            image.onmouseover = function() {
+                image.src = `/images/${character}-wave.jpg`
+            }
         }
     }
 })
